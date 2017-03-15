9ab97e35f68ec86789ce1a5a7ef41478d88813d8
Joel Andy <joel@waqastudios.com>
2017-03-14 21:54:03 -0500

    inicio}

---
+++ app/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import *
+from django.template import RequestContext
+from django.contrib.auth import *
+from django.contrib.auth.models import Group, User
+from django.core import serializers
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.db.models import Max,Count
+from django.core.mail import send_mail
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from app.models import *
+from django.db import transaction
+from django.contrib.auth.hashers import *
+from django.core.mail import send_mail
+from django.db import connection
+from django.utils.six.moves import range
+from django.http import StreamingHttpResponse
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import permission_required
+from django.views.decorators.csrf import csrf_exempt
+import time
+import collections
+import xlrd
+import json 
+import csv
+import simplejson
+import xlwt
+import requests
+import os
+
+
+from datetime import datetime,timedelta
+
+from django.contrib.auth import authenticate
+
+def home(request):
+
+
+	return render(request, 'home.html')
+
+
+def login(request):
+
+
+	return render(request, 'login.html')
+
+
